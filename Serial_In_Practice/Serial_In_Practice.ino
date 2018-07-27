325e0cdd096506bc77e6417b8ac70ce10757508f
OllieBck <job271@nyu.edu>
2018-07-27 12:57:57 -0400

    adds latest examples

--- Serial_In_Practice/Serial_In_Practice.ino
+++ Serial_In_Practice/Serial_In_Practice.ino
@@ -3,20 +3,27 @@
  void setup(){
   Serial.begin(9600);
   CircuitPlayground.begin();
-  while (Serial.available() <= 0) {
+  /*while (Serial.available() <= 0) {
     Serial.println("waiting"); // send a starting message
     delay(300);              // wait 1/3 second
-  }
+  }*/
 }
 
 void loop() {
   if (CircuitPlayground.leftButton() == true){
-    Serial.write('h');
+    Serial.print("h");
+    CircuitPlayground.redLED(true);
+    delay(100);
   }
   
   if (Serial.available() > 0) {
+    CircuitPlayground.redLED(true);
     char compData = Serial.read();
     Serial.print("Value of Data: ");
     Serial.println(compData);
+      delay(1000);
+  }
+  else{
+    CircuitPlayground.redLED(false);
   }
 }
